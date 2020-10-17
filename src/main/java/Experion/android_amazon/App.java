8e2f7a6e0ffd10ed2d9608ab3c29dc39fc6c4508
sri-qptl <srisabarinath.b@qapitol.com>
2020-10-17 17:19:02 +0530

    maven project

---
+++ src/main/java/Experion/android_amazon/App.java
@@ -0,0 +1,13 @@
+package Experion.android_amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
